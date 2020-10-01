aadcd57e1b77b3a63e2b9f546dc6d42cf64a4f4f
AJ Tran <ajtrandev@gmail.com>
2020-09-30 18:12:15 -0600

    Add sample users

--- server.js
+++ server.js
@@ -16,6 +16,40 @@ var schema = buildSchema(`
   }
 `);
 
+// Sample users
+var users = [
+  {
+    id: 1,
+    name: 'Brian',
+    age: '21',
+    shark: 'Great White Shark'
+  },
+  {
+    id: 2,
+    name: 'Kim',
+    age: '22',
+    shark: 'Whale Shark'
+  },
+  {
+    id: 3,
+    name: 'Faith',
+    age: '23',
+    shark: 'Hammerhead Shark'
+  },
+  {
+    id: 4,
+    name: 'Joseph',
+    age: '23',
+    shark: 'Tiger Shark'
+  },
+  {
+    id: 5,
+    name: 'Joy',
+    age: '25',
+    shark: 'Hammerhead Shark'
+  }
+]
+
 // Root resolver
 var root = {
   hello: () => 'Hello world!'
