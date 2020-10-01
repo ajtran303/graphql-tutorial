5410f4d7211919b7a8e66ee2f0df84e0af3524f1
AJ Tran <ajtrandev@gmail.com>
2020-09-30 18:13:58 -0600

    define resolvers

--- server.js
+++ server.js
@@ -50,9 +50,26 @@ var users = [
   }
 ]
 
+// Return a single user (based on id)
+var getUser = function(args) {
+  var userID = args.id;
+  return users.filter(user => user.id == userID)[0];
+}
+
+// Return a list of users (optional shark param)
+var retrieveUsers = function(args) {
+  if (args.shark) {
+    var shark = args.shark;
+    return users.filter(user => user.shark === shark);
+  } else {
+    return users;
+  }
+}
+
 // Root resolver
 var root = {
-  hello: () => 'Hello world!'
+  user: getUser, // return a user with a specific id
+  users: retrieveUsers
 };
 
 // Create express server and GraphQL endpoint
